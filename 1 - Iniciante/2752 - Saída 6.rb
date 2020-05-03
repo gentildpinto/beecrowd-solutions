f7f5d6a806f6e7a824e18f6ed945ada5060db237
GentilPinto <gentil.junior.gp@gmail.com>
2020-05-03 21:06:25 +0100

    2752 - Saida 6

---
+++ 1 - Iniciante/2752 - Saída 6.rb
@@ -0,0 +1,9 @@
+strg = "AMO FAZER EXERCICIO NO URI"
+puts "<% s>" % strg
+puts "<% 30s>" % strg
+puts "<% .20s>" % strg
+puts "<% -20s>" % strg
+puts "<% -30s>" % strg
+puts "<% .30s>" % strg
+puts "<% 30.20s>" % strg
+puts "<% -30.20s>" % strg
