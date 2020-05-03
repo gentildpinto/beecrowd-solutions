a015decc64d1b6fef984d4d581d49f0ad4bfa0c6
GentilPinto <gentil.junior.gp@gmail.com>
2020-05-04 00:09:38 +0100

    2747 - Saida 1 resolvido

---
+++ 1 - Iniciante/2747 - Saída 1.rb
@@ -0,0 +1,15 @@
+for i in (0...39)
+    print "-"
+end
+puts ""
+for i in (0...5)
+    print "|"
+    for j in (0...37)
+        print " "
+    end
+    puts "|"
+end
+for i in (0...39)
+    print "-"
+end
+puts ""
