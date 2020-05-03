d2e0f630e540b95781eb2102b1832fc6355a0c28
GentilPinto <gentil.junior.gp@gmail.com>
2020-05-03 20:46:00 +0100

    2754 - Saida 8 resolvido

---
+++ 1 - Iniciante/2754 - Saída 8.rb
@@ -0,0 +1,11 @@
+v1 = 234.345
+v2 = 45.698
+puts "%.6f - %.6f" % [v1, v2]
+puts "%.0f - %.0f" % [v1, v2]
+puts "%.1f - %.1f" % [v1, v2]
+puts "%.2f - %.2f" % [v1, v2]
+puts "%.3f - %.3f" % [v1, v2]
+puts "%e - %e" % [v1, v2]
+puts "%E - %E" % [v1, v2]
+puts "%.3f - %.3f" % [v1, v2]
+puts "%.3f - %.3f" % [v1, v2]
