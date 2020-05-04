b0b9517cab3b5881ba38ae7f536980b817116776
GentilPinto <gentil.junior.gp@gmail.com>
2020-05-04 21:19:25 +0100

    1050 - DDD resolvido

---
+++ 1 - Iniciante/1050 - DDD.rb
@@ -0,0 +1,20 @@
+N = gets.chomp.to_i
+if N == 61
+    puts "Brasilia"
+elsif N == 71
+    puts "Salvador"
+elsif N == 11
+    puts "Sao Paulo"
+elsif N == 21
+    puts "Rio de Janeiro"
+elsif N == 32
+    puts "Juiz de Fora"
+elsif N == 19
+    puts "Campinas"
+elsif N == 27
+    puts "Vitoria"
+elsif N == 31
+    puts "Belo Horizonte"
+else
+    puts "DDD nao cadastrado"
+end
