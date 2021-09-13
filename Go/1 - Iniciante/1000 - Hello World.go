b0ad182bc6d4a0e0f7041378aa108a97cfb2af9d
gentildpinto <gentil.junior.gp@gmail.com>
2021-09-13 12:35:33 +0100

    solved 1000 - Hello World in Go

---
+++ Go/1 - Iniciante/1000 - Hello World.go
@@ -0,0 +1,15 @@
+package main
+ 
+import (
+    "fmt"
+)
+ 
+func main() {
+
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    fmt.Println("Hello World!")
+}
